89e4904c33471739471911e4cf6e30ae4214f894
savissimo <simone.saviolo@gmail.com>
2025-10-22 00:43:58 +0200

    Fix mode della build

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,9 +2,10 @@ import { loadEnv } from 'vite'
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-const envvars = loadEnv(process.env.NODE_ENV, process.cwd())
+const envvars = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 const urlDomain = envvars.URL_DOMAIN
 const urlBasePath = envvars.URL_BASE_PATH
+console.log('Configuring site with URL_DOMAIN:', urlDomain, 'and URL_BASE_PATH:', urlBasePath)
 
 // https://astro.build/config
 export default defineConfig({
