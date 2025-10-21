2269f4111036b3f8ce0d4010328c96376f53327d
savissimo <simone.saviolo@gmail.com>
2025-10-21 23:37:37 +0200

    Variabili d'ambiente

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,9 +1,14 @@
+import { loadEnv } from 'vite'
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+const envvars = loadEnv(process.env.NODE_ENV, process.cwd())
+const urlDomain = envvars.URL_DOMAIN
+const urlBasePath = envvars.URL_BASE_PATH
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://simonesaviolo.github.io',
-	base: '/la-rete-www',
+	site: urlDomain ? `https://${urlDomain}` : undefined,
+	base: urlBasePath ?? '/',
 	trailingSlash: 'never',
-});
+})
