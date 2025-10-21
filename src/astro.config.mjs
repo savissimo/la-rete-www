51854f82314620b717c7e750ade6c8f1fba07387
savissimo <simone.saviolo@gmail.com>
2025-10-22 00:46:03 +0200

    Fix build

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 const envvars = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 const urlDomain = envvars.URL_DOMAIN
 const urlBasePath = envvars.URL_BASE_PATH
+console.log(`Mode: ${process.env.NODE_ENV}`)
 console.log('Configuring site with URL_DOMAIN:', urlDomain, 'and URL_BASE_PATH:', urlBasePath)
 
 // https://astro.build/config
