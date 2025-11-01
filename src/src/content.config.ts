446d666b5d398244288b41893c6828038d3ab3f8
savissimo <simone.saviolo@gmail.com>
2025-11-01 01:23:01 +0100

    Vercellesi nel Mondo - Interviste

--- src/src/content.config.ts
+++ src/src/content.config.ts
@@ -17,13 +17,31 @@ const vercellesiNelMondoDocuments = defineCollection({
 const vercellesiNelMondoPassaporto = defineCollection({
 	loader: file('./content/vercellesi-nel-mondo.json', { parser: (t) => JSON.parse(t).passaporto }),
 	schema: z.object({
+		id: z.number(),
 		image: z.string(),
 		name: z.string(),
 		link: z.string(),
 	}),
 })
 
+const vercellesiNelMondoInterviews = defineCollection({
+	loader: file('./content/vercellesi-nel-mondo.json', { parser: (t) => JSON.parse(t).interviews }),
+	schema: z.object({
+		id: z.number(),
+		image: z.string(),
+		name: z.string(),
+		surname: z.string(),
+		city: z.string(),
+		country: z.string(),
+		link: z.array(z.object({
+			label: z.string(),
+			url: z.string(),
+		})),
+	}),
+})
+
 export const collections = {
     'vercellesi-nel-mondo_documents': vercellesiNelMondoDocuments,
     'vercellesi-nel-mondo_passaporto': vercellesiNelMondoPassaporto,
+	'vercellesi-nel-mondo_interviews': vercellesiNelMondoInterviews,
 }
